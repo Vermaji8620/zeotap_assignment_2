4274106922b9bbd52d72f1fd449945219c123a34
AdityaRaj <xyz@gmai.com>
2024-10-23 22:59:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+// import weatherRoutes from "./routes/weatherRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// app.use("/api/weather", weatherRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
